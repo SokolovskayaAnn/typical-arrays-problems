e35a36dd886bb3d4650211fbd88be1124e6c91af
Anna Sokolovskaia <sokolovskaya.ann@mail.ru>
2020-10-03 21:58:15 +0300

    feat: implement max, min, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array != "undefined" && array != null && array.length != null
+  && array.length > 0) {
+    let minNumber = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] < minNumber) {
+        minNumber = array[i]; 
+      }
+    }
+    return minNumber;
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array !== "undefined" && array !== null && array.length !== null
+  && array.length > 0) {
+    let maxNumber = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] > maxNumber) {
+        maxNumber = array[i]; 
+      }
+    }
+    return maxNumber;
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array !== "undefined" && array !== null && array.length !== null
+  && array.length > 0) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum / array.length;
+  } else {
+    return 0;
+  }
 }
